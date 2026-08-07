331ab4b726681817bdbdfa89e232402d967f7e73
Fateha Jannat Sumaya <150549532+Fateha01@users.noreply.github.com>
2026-08-07 20:54:09 +0600

    Delete java directory

--- java/src/semantic/semantic.h
+++
@@ -1,7 +0,0 @@
-#ifndef SEMANTIC_H
-#define SEMANTIC_H
-#include "ast.h"
-#include "symbol_table.h"
-/* Returns 1 if no errors, 0 if any semantic error was found (also printed to stderr). */
-int semantic_analyze(ASTNode *program, SymbolTable *table);
-#endif
--- java/src/targetcode/targetcode.h
+++
@@ -1,9 +0,0 @@
-#ifndef TARGETCODE_H
-#define TARGETCODE_H
-
-/* Phase 6: Target Code Generation.
-   Translates optimized TAC lines into simple pseudo-assembly for a
-   hypothetical register machine and prints it to stdout. */
-void generate_target_code(char **lines, int count);
-
-#endif
